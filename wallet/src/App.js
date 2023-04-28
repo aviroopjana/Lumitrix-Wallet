de57af076ef64e37bf31a02554f856ca8c35f3b7
Aviroop Jana <aviroopjana@gmail.com>
2023-04-28 23:47:36 +0530

    update App.js

--- wallet/src/App.js
+++ wallet/src/App.js
@@ -80,7 +80,15 @@ function App() {
         :
         <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/recover" element={<RecoverAccount/>} />
+        <Route 
+          path="/recover" 
+          element={
+            <RecoverAccount
+              setSeedPhrase={setSeedPhrase}
+              setWallet={setWallet}
+            />
+          } 
+        />
         <Route 
           path="/yourwallet" 
           element={
