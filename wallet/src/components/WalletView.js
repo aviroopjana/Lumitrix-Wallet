5edd130a39a7979efeb48957fc97f35cdde29022
Aviroop Jana <aviroopjana@gmail.com>
2023-05-05 23:56:23 +0530

    update WalletView.js

--- wallet/src/components/WalletView.js
+++ wallet/src/components/WalletView.js
@@ -3,38 +3,38 @@ import { Divider, Tooltip, List, Avatar, Spin, Tabs, Input, Button} from "antd";
 import { LogoutOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
 
-// const tokens = [
-//   {
-//     symbol: "ETH",
-//     name: "Ethereum",
-//     balance: 100000000000,
-//     decimals: 18,
-//   },
-//   {
-//     symbol: "LINK",
-//     name: "Chainlink",
-//     balance: 100000000000,
-//     decimals: 18,
-//   },
-//   {
-//     symbol: "UNI",
-//     name: "Uniswap",
-//     balance: 100000000000,
-//     decimals: 18,
-//   },
-//   {
-//     symbol: "MATIC",
-//     name: "Polygon",
-//     balance: 100000000000,
-//     decimals: 18,
-//   },
-// ];
+const tokens = [
+  {
+    symbol: "ETH",
+    name: "Ethereum",
+    balance: 100000000000,
+    decimals: 18,
+  },
+  {
+    symbol: "LINK",
+    name: "Chainlink",
+    balance: 100000000000,
+    decimals: 18,
+  },
+  {
+    symbol: "UNI",
+    name: "Uniswap",
+    balance: 100000000000,
+    decimals: 18,
+  },
+  {
+    symbol: "MATIC",
+    name: "Polygon",
+    balance: 100000000000,
+    decimals: 18,
+  },
+];
 
-// const nfts = [
-//   "https://nft-preview-media.s3.us-east-1.amazonaws.com/evm/0x1/0xd774557b647330c91bf44cfeab205095f7e6c367/0xfb76f9ef3adabc27d77c615959f9e22dea24ac7d6a10af3458b3481e5f5e0f10/high.png",
-//   ,
-//   "https://nft-preview-media.s3.us-east-1.amazonaws.com/evm/0x1/0x749f5ddf5ab4c1f26f74560a78300563c34b417d/0x90cae88ffc909feab8e4df76abd0652dee98b7bffab29597d898260d91c20aa1/high.jpeg",
-// ];
+const nfts = [
+  "https://nft-preview-media.s3.us-east-1.amazonaws.com/evm/0x1/0xd774557b647330c91bf44cfeab205095f7e6c367/0xfb76f9ef3adabc27d77c615959f9e22dea24ac7d6a10af3458b3481e5f5e0f10/high.png",
+  ,
+  "https://nft-preview-media.s3.us-east-1.amazonaws.com/evm/0x1/0x749f5ddf5ab4c1f26f74560a78300563c34b417d/0x90cae88ffc909feab8e4df76abd0652dee98b7bffab29597d898260d91c20aa1/high.jpeg",
+];
 
 function WalletView( props ) {
 
@@ -46,7 +46,45 @@ function WalletView( props ) {
       label: 'Tokens',
       children: (
         <>
-          Tokens
+          {tokens ? (
+            <>
+              <List
+                bordered
+                itemLayout="horizontal"
+                dataSource={tokens}
+                renderItem={(item,index) => (
+                  <List.Item style={{ textAlign: "left" }} >
+                    <List.Item.Meta
+                      avatar={ <Avatar src={item.logo} /> }
+                      title={item.symbol}
+                      description={item.name}
+                    />
+                    <div>
+                      {(
+                        Number(item.balance) /
+                        10 ** Number(item.decimals)
+                      ).toFixed(2)} {" "}
+                      Tokens
+                    </div>                   
+                  </List.Item>
+                )}
+              />
+            </>
+          ):(
+            <>
+              <span>You seem to not have any tokens yet</span>
+              <p className="frontPageButtom" >
+                Find alt coin gems:{""}
+              </p>
+              <a 
+                href="https://moralismoney.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                money.moralis.io
+              </a>
+            </>
+          )}
         </>
       ),
     },
@@ -55,7 +93,38 @@ function WalletView( props ) {
       label: 'NFTs',
       children: (
         <>
-          NFTs
+          {nfts ? (
+            <>
+              {nfts.map((e,i)=> {
+                return (
+                  <>
+                    {e && (
+                      <img
+                        key={i}
+                        className="nftImage"
+                        alt="nftImage"
+                        src={e}
+                      />
+                    )}
+                  </>
+                )
+              })}
+            </>
+          ):(
+            <>
+            <span>You seem to not have any nfts yet</span>
+              <p className="frontPageButtom" >
+                Find alt coin gems:{""}
+              </p>
+              <a 
+                href="https://moralismoney.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                money.moralis.io
+              </a>
+            </>
+          )}
         </>
       ),
     },
