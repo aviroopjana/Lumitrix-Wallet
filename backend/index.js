a4704dcbac1eabab22ef533c98c4839a7624b7b3
Aviroop Jana <aviroopjana@gmail.com>
2023-05-08 23:59:18 +0530

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,20 +23,25 @@ app.get("/getTokens", async (req, res) => {
     mediaItems: true,
   });
 
+  const myNfts = nfts.raw.result.map((e, i) => {
+    if (e?.media?.media_collection?.high?.url && !e.possible_spam && (e?.media?.category !== "video") ) {
+      return e["media"]["media_collection"]["high"]["url"];
+    }
+  })
+
   const balance = await Moralis.EvmApi.balance.getNativeBalance({
     chain: chain,
-    address: userAddress,
+    address: userAddress
   });
 
   const jsonResponse = {
     tokens: tokens.raw,
-    nfts: nfts.raw,
-    balance: balance.raw.balance,
+    nfts: myNfts,
+    balance: balance.raw.balance / (10 ** 18)
   }
 
   return res.status(200).json(jsonResponse);
 });
-
 
 Moralis.start({
   apiKey: process.env.MORALIS_KEY,
