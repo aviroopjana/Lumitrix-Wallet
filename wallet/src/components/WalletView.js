ad565f7ca03384f5c632c3433862470627f5b35b
Aviroop Jana <aviroopjana@gmail.com>
2023-05-12 23:42:46 +0530

    update WalletView.js

--- wallet/src/components/WalletView.js
+++ wallet/src/components/WalletView.js
@@ -2,43 +2,22 @@ import React, { useEffect, useState } from "react";
 import { Divider, Tooltip, List, Avatar, Spin, Tabs, Input, Button} from "antd";
 import { LogoutOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
+import  axios from "axios";
+import { CHAINS_CONFIG } from "../chains";
 
-const tokens = [
-  {
-    symbol: "ETH",
-    name: "Ethereum",
-    balance: 100000000000,
-    decimals: 18,
-  },
-  {
-    symbol: "LINK",
-    name: "Chainlink",
-    balance: 100000000000,
-    decimals: 18,
-  },
-  {
-    symbol: "UNI",
-    name: "Uniswap",
-    balance: 100000000000,
-    decimals: 18,
-  },
-  {
-    symbol: "MATIC",
-    name: "Polygon",
-    balance: 100000000000,
-    decimals: 18,
-  },
-];
 
-const nfts = [
-  "https://nft-preview-media.s3.us-east-1.amazonaws.com/evm/0x1/0xd774557b647330c91bf44cfeab205095f7e6c367/0xfb76f9ef3adabc27d77c615959f9e22dea24ac7d6a10af3458b3481e5f5e0f10/high.png",
-  ,
-  "https://nft-preview-media.s3.us-east-1.amazonaws.com/evm/0x1/0x749f5ddf5ab4c1f26f74560a78300563c34b417d/0x90cae88ffc909feab8e4df76abd0652dee98b7bffab29597d898260d91c20aa1/high.jpeg",
-];
-
-function WalletView( props ) {
-
+function WalletView({
+  wallet,
+  setWallet,
+  seedPhrase,
+  setSeedPhrase,
+  selectedChain,
+}) {
   const navigate = useNavigate();
+  const [nfts, setNfts] = useState(null);
+  const [tokens, setTokens] = useState(null);
+  const [balance, setBalance] = useState(0);
+  const [fetching, setFetching] = useState(true);
 
   const items = [
     {
@@ -75,14 +54,14 @@ function WalletView( props ) {
               <span>You seem to not have any tokens yet</span>
               <p className="frontPageButtom" >
                 Find alt coin gems:{""}
+                <a 
+                  href="https://moralismoney.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  money.moralis.io
+                </a>
               </p>
-              <a 
-                href="https://moralismoney.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                money.moralis.io
-              </a>
             </>
           )}
         </>
@@ -107,22 +86,22 @@ function WalletView( props ) {
                       />
                     )}
                   </>
-                )
+                );
               })}
             </>
           ):(
             <>
             <span>You seem to not have any nfts yet</span>
               <p className="frontPageButtom" >
-                Find alt coin gems:{""}
+                Find alt coin gems :{" "}
+                  <a 
+                  href="https://moralismoney.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  money.moralis.io
+                </a>
               </p>
-              <a 
-                href="https://moralismoney.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                money.moralis.io
-              </a>
             </>
           )}
         </>
@@ -131,19 +110,65 @@ function WalletView( props ) {
     {
       key: "1",
       label: 'Transfer',
-      children: (
+      children:
         <>
-          Transfer
+          <h3>Native Balance </h3>
+          <h1>
+            {balance.toFixed(2)} 
+          </h1>
         </>
-      ),
     }
   ]
 
+  async function getAccountTokens() {
+    setFetching(true);
+
+    const res = await axios.get(`http://localhost:3001/getTokens`, {
+      params: {
+        userAddress: wallet,
+        chain: selectedChain,
+      },
+    });
+
+    const response = res.data;
+
+    if (response.tokens.length > 0) {
+      setTokens(response.tokens);
+    }
+
+    if (response.nfts.length > 0) {
+      setNfts(response.nfts);
+    }
+
+    setBalance(response.balance);
+
+    setFetching(false);
+  }
+
   function logout() {
-    props.setSeedPhrase(null);
-    props.setWallet(null);
+    setSeedPhrase(null);
+    setWallet(null);
+    setNfts(null);
+    setTokens(null);
+    setBalance(0);
     navigate("/");
   }
+
+  useEffect(() => {
+    if (!wallet || !selectedChain) return;
+    setNfts(null);
+    setTokens(null);
+    setBalance(0);
+    getAccountTokens();
+  }, []);
+
+  useEffect(() => {
+    if (!wallet) return;
+    setNfts(null);
+    setTokens(null);
+    setBalance(0);
+    getAccountTokens();
+  }, [selectedChain]);
  
   return (
     <>
@@ -155,22 +180,27 @@ function WalletView( props ) {
             Your Wallet
           </div>
           <Tooltip 
-            title={props.wallet} 
+            title={wallet} 
             style={{
               color: "#f9f9f9"
             }}
           >
-            {props.wallet.slice(0,4)}...{props.wallet.slice(38)}
+          <div>
+            {wallet.slice(0,4)}...{wallet.slice(38)}
+          </div>           
           </Tooltip>
           <Divider
             style={{
               borderBottom: "1px solid #ffff"
             }}
           />
-          <Tabs defaultActiveKey={items[0].key} items={items} className="walletView" />
+          {fetching ? (
+          <Spin />
+        ) : (
+          <Tabs defaultActiveKey="1" items={items} className="walletView" />
+        )}
       </div>
     </>
   );
 }
-
 export default WalletView;
