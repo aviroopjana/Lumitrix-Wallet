55d30df61e44020a83ca6f3aa0a212de66cd2f08
Aviroop Jana <aviroopjana@gmail.com>
2023-05-14 23:25:32 +0530

    update chains.js

--- wallet/src/chains.js
+++ wallet/src/chains.js
@@ -8,11 +8,27 @@ const Ethereum = {
 const MumbaiTestnet = {
     hex: '0x13881',
     name: 'Mumbai Testnet',
+    rpcUrl: 'https://polygon-mumbai.g.alchemy.com/v2/MwyIddfuPkTQik7FqWYJCtxd1tcyxh1q',
+    ticker: "MATIC"
+};
+
+const Polygon = {
+    hex: '0x89',
+    name: 'Polygon',
     rpcUrl: '',
     ticker: "MATIC"
+}
+
+const Avalanche = {
+    hex: '0xa86a',
+    name: 'Avalanche',
+    rpcUrl: '',
+    ticker: 'AVAX'
 };
 
 export const CHAINS_CONFIG = {
     "0x1": Ethereum,
     "0x13881": MumbaiTestnet,
+    "0xa86a": Avalanche,
+    "0x89": Polygon
 };
